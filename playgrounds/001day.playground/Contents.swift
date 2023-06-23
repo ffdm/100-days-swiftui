fec01cd3aed3008f2088e7dfa8ed656fe64598f2
Finn <finn@0587449384.wireless.umich.net>
2023-06-22 20:15:47 -0400

    First day, notes and playground

---
+++ playgrounds/001day.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+let greeting = "Hello, playground"
+print(greeting.count)
+print(greeting.uppercased())
+print(greeting.hasPrefix("Hel"))
+print(greeting.hasPrefix("pla"))
+var n1 = 0
+var n2 = 1
+print(n1.isMultiple(of: n2))
+print(n2.isMultiple(of: n1))
+var n3 = 0.1
+print(n2+0.1)
+print(n3+2)
+print(n2+n3)
