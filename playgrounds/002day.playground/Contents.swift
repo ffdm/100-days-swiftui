27e9b874cd79836f37e1549eae8b0c07650d8788
Finn <finn@Finns-MacBook-Pro.local>
2023-06-23 14:38:43 -0400

    Day 2

---
+++ playgrounds/002day.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+let name = "Bill"
+let age = 25
+let message = "Hi, I \(name) and I \(age) years old"
+print(message)
+print("Hello \(5*5)")
+// Checkpoint 1
+let celcius = 30.0
+let farenheit = (celcius*9)/5 + 32
+print(farenheit)
